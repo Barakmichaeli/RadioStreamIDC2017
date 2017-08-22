9938c21e8025fe14cdb06bc9442f15cad69ebcd0
Alon Weiner <alon7771@gmail.com>
2017-08-22 14:40:21 +0300

    register and login lines fixed left

--- src/Register.js
+++ src/Register.js
@@ -12,7 +12,7 @@ class Register extends Component {
 
     render() {
         return (
-            
+
             <div className="container">
                 <div className="login-header">
                     <h1>RadioStream Register</h1>
